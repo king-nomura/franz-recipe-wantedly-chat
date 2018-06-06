9d708a88220a75087081549230dfbe72b4b7ad0b
nomura masayuki <king.nomura@gmail.com>
2018-06-06 09:22:39 +0900

    ログインが要求されたときに別URLに遷移するように修正

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-module.exports = Franz => Franz;
+'use strict';
+
+module.exports = Franz => {
+	return class WantedlyChat extends Franz {
+		constructor(...args) {
+			var _temp;
+			
+			return _temp = super(...args), this.events = {
+				'console-message': '_consoleMessage'
+
+			}, _temp;
+		}
+		
+		_consoleMessage(event) {
+			if (event.message.indexOf("electron-safe-ipc:///message") >= 0) {
+				if (event.message.indexOf("channel=signIn") >= 0) {
+					event.target.src = 'https://m.wantedly.com/';
+				}
+			}
+		}
+	};
+};
